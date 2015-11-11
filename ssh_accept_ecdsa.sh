4749a47c20ea57607f899ad6031ad182f3d7859b
jake9050 <jan.duprez@gmail.com>
2015-11-11 02:31:51 +0100

    Update ssh_accept_ecdsa.sh
    Parametrized username

--- ssh_accept_ecdsa.sh
+++ ssh_accept_ecdsa.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+-z $1 && exit 1
+user="$1"
+
 hosts=(  1.2.3.4 put.other.ip.here dommainnames.supported.aswell )
 for host in ${hosts[@]}; do  echo "Registering at $host"
 
-  ssh root@$host 'pwd'
+  ssh "$user"@"$host" 'pwd'
   #Answer pesky ecdsa dialog
   expect "fingerprint"
   send "yes\n"
